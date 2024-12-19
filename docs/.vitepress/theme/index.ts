0a42d96a62c80bfac5a98a52efd59ad8b2a0fa2d
Eugenio Topa <xeyos88@gmail.com>
2024-12-19 20:24:42 +0100

    [UPD] Removed unused import in docs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,13 +2,10 @@ import DefaultTheme from 'vitepress/theme'
 import { defineClientComponent } from 'vitepress'
 import './custom.css'
 import dayjs from "dayjs"
-import isoWeek from "dayjs/plugin/isoWeek"
 import isSameOrBefore from "dayjs/plugin/isSameOrBefore.js"
 import isSameOrAfter from "dayjs/plugin/isSameOrAfter.js"
 import isBetween from "dayjs/plugin/isBetween.js"
-import weekOfYear from "dayjs/plugin/weekOfYear"
-import advancedFormat from "dayjs/plugin/advancedFormat"
-import customParseFormat from "dayjs/plugin/customParseFormat.js"
+
 
 export function extendDayjs() {
   dayjs.extend(isSameOrBefore)
